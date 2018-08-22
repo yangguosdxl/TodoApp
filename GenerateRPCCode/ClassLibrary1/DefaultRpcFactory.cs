e5cfe432215375c59da315152282d2ae9b53ebc3
yangguosdxl <591188898@qq.com>
2018-08-22 19:10:10 +0800

    no message

---
+++ GenerateRPCCode/ClassLibrary1/DefaultRpcFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolRpcInterface
+{
+    public class DefaultRpcFactory : IRpcFactory
+    {
+        public ISerializer serializer { get; set; }
+        public ICallAsync callAsync { get; set; }
+
+        public IServiceProvider serviceProvider { get; set; }
+
+        public T Get<T>()
+        {
+            object o;
+            if (m_RpcServices.TryGetValue(typeof(T)))
+        }
+    }
+}
---
+++ GenerateRPCCode/ClassLibrary1/IRpcFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolRpcInterface
+{
+    public interface IRpcFactory
+    {
+        ISerializer serializer { get; set; }
+        ICallAsync callAsync { get; set; }
+        T Get<T>();
+    }
+}
