ab718e41d209b23993ebc658559604d346fc9f01
yangguosdxl <591188898@qq.com>
2018-09-09 10:49:08 +0800

    异步编程测试

---
+++ TestAsync/TestAsync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
