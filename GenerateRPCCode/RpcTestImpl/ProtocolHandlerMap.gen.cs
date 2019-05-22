d88f87e379f454c6584e21abb3b61225c0ba7b09
yangguosdxl <591188898@qq.com>
2019-05-23 07:42:21 +0800

    11

---
+++ GenerateRPCCode/RpcTestImpl/ProtocolHandlerMap.gen.cs
@@ -0,0 +1,16 @@
+﻿using CSRPC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpcTestImpl
+{
+    public static partial class ProtocolHandlerMap
+    {
+        static ProtocolHandlerMap()
+        {
+            s_aProtocolHandlers = new Cool.Interface.Rpc.ProtocolHandler[(int)ProtoID.COUNT];
+            s_aProtocolDeserializers = new Cool.Interface.Rpc.ProtocolDeserializer[(int)ProtoID.COUNT];
+        }
+    }
+}
