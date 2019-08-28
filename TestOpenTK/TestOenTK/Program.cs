d2de36b84be4a3f5ff0fe78a3ade39faa193a5df
yangguosdxl <591188898@qq.com>
2019-08-28 18:02:17 +0800

    OpenTk测试

---
+++ TestOpenTK/TestOenTK/Program.cs
@@ -0,0 +1,100 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES20;
+using OpenTK.Input;
+using System;
+using System.IO;
+
+namespace TestOenTK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Curr work path: " + Directory.GetCurrentDirectory());
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                //Run takes a double, which is how many frames per second it should strive to reach.
+                //You can leave that out and it'll just update as fast as the hardware will allow it.
+                game.Run(60.0);
+            }
+        }
+    }
+
+    class Game : GameWindow
+    {
+        int VertexBufferObject;
+
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        Shader shader;
+
+        public Game(int width, int height, string title) : 
+            base(width, height, GraphicsMode.Default, title)
+        { }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            //Get the state of the keyboard this frame
+            KeyboardState input = Keyboard.GetState();
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            //Code goes here
+            shader = new Shader("shader.vert", "shader.frag");
+            VertexBufferObject = GL.GenBuffer();
+
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+
+            shader.Dispose();
+
+            base.OnUnload(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            shader.Use();
+            
+
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+    }
+}
---
+++ TestOpenTK/TestOenTK/Shader.cs
@@ -0,0 +1,97 @@
+﻿using OpenTK.Graphics.ES20;
+using System;
+using System.IO;
+using System.Text;
+
+public class Shader : IDisposable
+{
+    int Handle;
+
+    public Shader(string vertexPath, string fragmentPath)
+    {
+        string VertexShaderSource;
+
+        using (StreamReader reader = new StreamReader(vertexPath, Encoding.UTF8))
+        {
+            VertexShaderSource = reader.ReadToEnd();
+        }
+
+        string FragmentShaderSource;
+
+        using (StreamReader reader = new StreamReader(fragmentPath, Encoding.UTF8))
+        {
+            FragmentShaderSource = reader.ReadToEnd();
+        }
+
+        int VertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(VertexShader, VertexShaderSource);
+
+        int FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+        GL.CompileShader(VertexShader);
+
+        string infoLogVert = GL.GetShaderInfoLog(VertexShader);
+        if (infoLogVert != System.String.Empty)
+            System.Console.WriteLine(infoLogVert);
+
+        GL.CompileShader(FragmentShader);
+
+        string infoLogFrag = GL.GetShaderInfoLog(FragmentShader);
+
+        if (infoLogFrag != System.String.Empty)
+            System.Console.WriteLine(infoLogFrag);
+
+        Handle = GL.CreateProgram();
+
+        GL.AttachShader(Handle, VertexShader);
+        GL.AttachShader(Handle, FragmentShader);
+
+        GL.LinkProgram(Handle);
+
+        GL.DetachShader(Handle, VertexShader);
+        GL.DetachShader(Handle, FragmentShader);
+        GL.DeleteShader(FragmentShader);
+        GL.DeleteShader(VertexShader);
+    }
+
+    public void Use()
+    {
+        GL.UseProgram(Handle);
+    }
+
+    #region IDisposable Support
+    private bool disposedValue = false; // 要检测冗余调用
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: 释放托管状态(托管对象)。
+            }
+
+            GL.DeleteProgram(Handle);
+
+            disposedValue = true;
+        }
+    }
+
+    // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
+    ~Shader()
+    {
+      // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+      Dispose(false);
+    }
+
+    // 添加此代码以正确实现可处置模式。
+    public void Dispose()
+    {
+        // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+        Dispose(true);
+        // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
+        GC.SuppressFinalize(this);
+    }
+    #endregion
+}
