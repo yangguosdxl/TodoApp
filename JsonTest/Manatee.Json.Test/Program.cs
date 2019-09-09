69a48f14ffe56823de01edbdf866af5df8f01d2a
yangguosdxl <591188898@qq.com>
2019-09-10 07:55:33 +0800

    json test

---
+++ JsonTest/Manatee.Json.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Manatee.Json.Test
+{
+    class Program
+    {
+        class Table
+        {
+            public int a;
+        }
+
+        class TableCollection
+        {
+            public Dictionary<int, Table> tables = new Dictionary<int, Table>();
+        }
+        static void Main(string[] args)
+        {
+            
+            var serializer = new JsonSerializer();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
