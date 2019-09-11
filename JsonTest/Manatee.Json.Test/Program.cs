69087ef55eb715a839afdb2e6375354b3bb476d8
yangguosdxl <591188898@qq.com>
2019-09-12 07:50:46 +0800

    test

--- JsonTest/Manatee.Json.Test/Program.cs
+++ JsonTest/Manatee.Json.Test/Program.cs
@@ -9,20 +9,22 @@ using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.Utilities;
 using System.Xml;
 using System.IO;
+using System.Xml.Serialization;
+using System.Text;
 
 namespace Manatee.Json.Test
 {
-    internal static class Program
+    public static class Program
     {
-        private class Table
+        public class Table
         {
             [Range(10, 100)]
             public int a;
         }
 
-        private class TableCollection
+        public class TableCollection
         {
-            public Dictionary<int, Table> tables = new Dictionary<int, Table>();
+            public List<Table> tables = new List<Table>();
         }
 
         private static void Main(string[] args)
@@ -33,21 +35,31 @@ namespace Manatee.Json.Test
             }
 
             TableCollection tc = new TableCollection();
-            tc.tables.Add(1, new Table(){a = 1});
-            tc.tables.Add(2, new Table(){a = 2});
+            tc.tables.Add(new Table(){a = 1});
+            tc.tables.Add(new Table(){a = 2});
 
-            string json = JsonConvert.SerializeObject(tc);
+            var xmlSerializer = new XmlSerializer(tc.tables.GetType());
+            var str = new StringBuilder();
+
+            xmlSerializer.Serialize(new StringWriter(str), tc.tables);
+
+            var xml = str.ToString();
+            File.WriteAllText("tables.xml", xml);
+
+            Console.WriteLine("xml----------\n" + xml);
+
+            string json = JsonConvert.SerializeObject(tc.tables);
 
             File.WriteAllText("tables.json", json);
 
             Console.WriteLine("json----------\n" + json);
 
-            XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(json);
-            doc.Save("tables.xml");
+            //XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(json);
+            //doc.Save("tables.xml");
 
-            string json2 = JsonConvert.SerializeXmlNode(doc);
+            //string json2 = JsonConvert.SerializeXmlNode(doc);
 
-            Console.WriteLine("json2----------\n" + json2);
+            //Console.WriteLine("json2----------\n" + json2);
 
             JSchemaGenerator gen = new JSchemaGenerator();
             JSchema schema = gen.Generate(typeof(TableCollection));
