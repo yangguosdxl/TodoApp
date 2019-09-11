31881b3234780ea04bff4a8d53b0a98ea728f667
yangguosdxl <591188898@qq.com>
2019-09-11 18:01:45 +0800

    test

--- JsonTest/Manatee.Json.Test/Program.cs
+++ JsonTest/Manatee.Json.Test/Program.cs
@@ -8,32 +8,59 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.Utilities;
 using System.Xml;
+using System.IO;
 
 namespace Manatee.Json.Test
 {
-    class Program
+    internal static class Program
     {
-        class Table
+        private class Table
         {
             [Range(10, 100)]
             public int a;
         }
 
-        class TableCollection
+        private class TableCollection
         {
             public Dictionary<int, Table> tables = new Dictionary<int, Table>();
         }
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
             TableCollection tc = new TableCollection();
+            tc.tables.Add(1, new Table(){a = 1});
+            tc.tables.Add(2, new Table(){a = 2});
 
             string json = JsonConvert.SerializeObject(tc);
 
-            Console.WriteLine(json);
+            File.WriteAllText("tables.json", json);
+
+            Console.WriteLine("json----------\n" + json);
 
             XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(json);
+            doc.Save("tables.xml");
 
-            Console.WriteLine(doc.ToString());
+            string json2 = JsonConvert.SerializeXmlNode(doc);
+
+            Console.WriteLine("json2----------\n" + json2);
+
+            JSchemaGenerator gen = new JSchemaGenerator();
+            JSchema schema = gen.Generate(typeof(TableCollection));
+            using (StreamWriter sw = new StreamWriter("tables.schema"))
+            {
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    schema.WriteTo(writer);
+                }
+            }
+
+            
+            Console.WriteLine("tables.schema----------\n" + schema.ToString());
 
             Console.WriteLine("Hello World!");
         }
